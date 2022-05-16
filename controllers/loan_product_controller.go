026afe096263bbc4c12464762676a97fa07404df
codeninjaug <segayiandrew@gmail.com>
2022-05-16 17:01:01 +0300

    added routes to the main function

--- controllers/loan_product_controller.go
+++ controllers/loan_product_controller.go
@@ -14,7 +14,7 @@ type LoanProductRepo struct {
 	Db *gorm.DB
 }
 
-func New() *LoanProductRepo {
+func NewLoanProduct() *LoanProductRepo {
 	db := database.InitDB()
 	db.AutoMigrate(&models.LoanProduct{})
 	return &LoanProductRepo{Db: db}
@@ -32,64 +32,65 @@ func (repo *LoanProductRepo) CreateLoanProduct(c *gin.Context) {
 	c.JSON(http.StatusOK, loanproduct)
 }
 
-//getting loan products from the loanproduct model 
-func (repo *LoanProductRepo) GetLoanProducts(c *gin.Context){
-    var loanproducts []models.LoanProduct
-    err:= models.GetLoanProducts(repo.Db, &loanproducts)
-	if err!=nil{
-		c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
+//getting loan products from the loanproduct model
+func (repo *LoanProductRepo) GetLoanProducts(c *gin.Context) {
+	var loanproducts []models.LoanProduct
+	err := models.GetLoanProducts(repo.Db, &loanproducts)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
 	}
-	c.JSON(http.StatusOK,loanproducts)
+	c.JSON(http.StatusOK, loanproducts)
 }
 
 //getting a single loan from the database
-func(repo *LoanProductRepo)GetLoanProduct(c *gin.Context){
-       id,_ := c.Params.Get("id")
-	   var loan_product models.LoanProduct
-	   err:= models.GetLoanProduct(repo.Db,&loan_product,id)
-	   if err!=nil{
-		  if errors.Is(err , gorm.ErrRecordNotFound){
-			  c.AbortWithStatus(http.StatusNotFound)
-			  return
-		  }
-		    c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
-		    return
-	   }
-	   c.JSON(http.StatusOK,loan_product)
+func (repo *LoanProductRepo) GetLoanProduct(c *gin.Context) {
+	id, _ := c.Params.Get("id")
+	var loan_product models.LoanProduct
+	err := models.GetLoanProduct(repo.Db, &loan_product, id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, loan_product)
 }
 
-//update loanproduct 
-func (repo *LoanProductRepo)UpdateLoanProduct(c *gin.Context){
-    var loan_product models.LoanProduct
-	id,_:= c.Params.Get("id")
-	err := models.GetLoanProduct(repo.Db,&loan_product,id)
-	if err!=nil{
-		if errors.Is(err, gorm.ErrRecordNotFound){
+//update loanproduct
+func (repo *LoanProductRepo) UpdateLoanProduct(c *gin.Context) {
+	var loan_product models.LoanProduct
+	id, _ := c.Params.Get("id")
+	err := models.GetLoanProduct(repo.Db, &loan_product, id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
 			c.AbortWithStatus(http.StatusNotFound)
-			return 
+			return
 		}
-		c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
 		return
 	}
 	c.BindJSON(&loan_product)
-	err= models.UpdateLoanProduct(repo.Db,&loan_product)
-	if err!=nil{
-		c.JSON(http.StatusOK,loan_product)
+	err = models.UpdateLoanProduct(repo.Db, &loan_product)
+	if err != nil {
+		c.JSON(http.StatusOK, loan_product)
 	}
 }
 
 //delete a loan product
-func(repo *LoanProductRepo)DeleteLoanProduct(c *gin.Context){
-   var loan_product models.LoanProduct
-   id,_:= c.Params.Get("id")
-   err:= models.DeleteLoanProduct(repo.Db,&loan_product,id)
-   if err !=nil{
-	   c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
-	   return
-   }
-   c.JSON(http.StatusOK,gin.H{"message":"LoanProduct  Deleted Successfully"})
+func (repo *LoanProductRepo) DeleteLoanProduct(c *gin.Context) {
+	var loan_product models.LoanProduct
+	id, _ := c.Params.Get("id")
+	err := models.DeleteLoanProduct(repo.Db, &loan_product, id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "LoanProduct  Deleted Successfully"})
 }
+
 // func GetLoanProducts() gin.HandlerFunc {
 // 	return func(ctx *gin.Context) {
 
--- main.go
+++ main.go
@@ -1,48 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/LoanMgtGo/database"
-	"github.com/LoanMgtGo/routes"
+	"net/http"
+    "github.com/LoanMgtGo/controllers"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 )
 
 var err error
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000"
-	}
+	r := setUpRouter()
+	_ = r.Run(":8090")
+}
 
-	router := gin.New()
-	database.DB, err = gorm.Open("mysql", database.DB_url(database.InitDatabase()))
-	if err != nil {
-		fmt.Println("status", err)
-	}
-	defer database.DB.Close()
-	router.Use(gin.Logger())
-	routes.UserRoutes(router)
-	//router.Use(middleware.Authentication())
-	routes.AssetRoutes(router)
-	routes.AssetTypeRoutes(router)
-	routes.ChartsAccountsRoutes(router)
-	routes.ClientRoutes(router)
-	routes.ExpensesRoutes(router)
-	routes.IncomeRoutes(router)
-	routes.JourneyEntryRoutes(router)
-	routes.LoanRoutes(router)
-	routes.LoanChargeRoutes(router)
-	routes.LoanProductRoutes(router)
-	routes.SavingsRoutes(router)
-	routes.SavingsChargesRoutes(router)
-	routes.SavingsProductsRoutes(router)
-	routes.SavingsTransactionsRoutes(router)
-	routes.ChartsAccountsRoutes(router)
-	routes.ShareRoutes(router)
-	routes.ShareChargesRoutes(router)
-	routes.ShareProductRoutes(router)
+func setUpRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "pong")
+	})
+
+    loan_product_repo := controllers.NewLoanProduct()
+	r.GET("/loan/products",loan_product_repo.GetLoanProducts)
+	r.POST("/create/product",loan_product_repo.CreateLoanProduct)
+	r.GET("/loan/product/:id",loan_product_repo.GetLoanProduct)
+	r.PUT("/loan/product/:id",loan_product_repo.UpdateLoanProduct)
+	r.DELETE("/loan/product/:id",loan_product_repo.DeleteLoanProduct)
+	return r
 }
