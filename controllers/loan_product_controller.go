438f23a57c6d40b201a2d0b9a9e6c6386d393944
codeninjaug <segayiandrew@gmail.com>
2022-05-16 15:28:06 +0300

    crud controller loan product

--- controllers/loan_product_controller.go
+++ controllers/loan_product_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/LoanMgtGo/database"
@@ -31,6 +32,64 @@ func (repo *LoanProductRepo) CreateLoanProduct(c *gin.Context) {
 	c.JSON(http.StatusOK, loanproduct)
 }
 
+//getting loan products from the loanproduct model 
+func (repo *LoanProductRepo) GetLoanProducts(c *gin.Context){
+    var loanproducts []models.LoanProduct
+    err:= models.GetLoanProducts(repo.Db, &loanproducts)
+	if err!=nil{
+		c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
+		return
+	}
+	c.JSON(http.StatusOK,loanproducts)
+}
+
+//getting a single loan from the database
+func(repo *LoanProductRepo)GetLoanProduct(c *gin.Context){
+       id,_ := c.Params.Get("id")
+	   var loan_product models.LoanProduct
+	   err:= models.GetLoanProduct(repo.Db,&loan_product,id)
+	   if err!=nil{
+		  if errors.Is(err , gorm.ErrRecordNotFound){
+			  c.AbortWithStatus(http.StatusNotFound)
+			  return
+		  }
+		    c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
+		    return
+	   }
+	   c.JSON(http.StatusOK,loan_product)
+}
+
+//update loanproduct 
+func (repo *LoanProductRepo)UpdateLoanProduct(c *gin.Context){
+    var loan_product models.LoanProduct
+	id,_:= c.Params.Get("id")
+	err := models.GetLoanProduct(repo.Db,&loan_product,id)
+	if err!=nil{
+		if errors.Is(err, gorm.ErrRecordNotFound){
+			c.AbortWithStatus(http.StatusNotFound)
+			return 
+		}
+		c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
+		return
+	}
+	c.BindJSON(&loan_product)
+	err= models.UpdateLoanProduct(repo.Db,&loan_product)
+	if err!=nil{
+		c.JSON(http.StatusOK,loan_product)
+	}
+}
+
+//delete a loan product
+func(repo *LoanProductRepo)DeleteLoanProduct(c *gin.Context){
+   var loan_product models.LoanProduct
+   id,_:= c.Params.Get("id")
+   err:= models.DeleteLoanProduct(repo.Db,&loan_product,id)
+   if err !=nil{
+	   c.AbortWithStatusJSON(http.StatusInternalServerError,gin.H{"error":err})
+	   return
+   }
+   c.JSON(http.StatusOK,gin.H{"message":"LoanProduct  Deleted Successfully"})
+}
 // func GetLoanProducts() gin.HandlerFunc {
 // 	return func(ctx *gin.Context) {
 
--- models/LoanProduct.go
+++ models/LoanProduct.go
@@ -7,17 +7,16 @@ import (
 type Fund string
 
 const (
-	Donorfund Fund = "Donorfund"
-	Clientfund    Fund = "Clientfund"
+	Donorfund  Fund = "Donorfund"
+	Clientfund Fund = "Clientfund"
 )
 
 type AutoDisburse string
 
- const (
-   Yes AutoDisburse = "Yes"
-   No  AutoDisburse = "No"
+const (
+	Yes AutoDisburse = "Yes"
+	No  AutoDisburse = "No"
 )
-
 
 type AccountingRule string
 
@@ -40,7 +39,6 @@ const (
 	EqualPrincipalPayments AmortizationMethod = "EqualPrincipalPayments"
 )
 
-
 type RepaymentType string
 
 const (
@@ -58,77 +56,75 @@ const (
 
 type LoanProduct struct {
 	gorm.Model
-	Name                              string   `json:"name"`
-	ShortName                         string   `json:"shortname"`
-	Description                       string   `json:"description"`
-	Fund                              Fund     `json:"fund"`
-	Currency                          Currency `json:"currency"`
-	DecimalPlaces                     float64  `json:"decimal"`
-	DefaultPrincipal                  float64  `json:"default_principal"`
-	MinimumPrincipal                  float64  `json:"minimum_princial"`
-	MaximumPrincipal                  float64  `json:"maximum_principal"`
-	DefaultLoanTerm                   float64  `json:"default_loan_term"`
-	MinimumLoanTerm                   float64  `json:"minimum_loan_term"`
-	MaximumLoanTerm                   float64  `json:"maximum_loan_term"`
-	RepaymentFrequency                float64  `json:"repayment_frequency"`
-	Type                              RepaymentType `json:"repayment_type"`
-	DefaultInterestRate               float64  `json:"default_interest_rate"`
-	MinimumInterestRate               float64  `json:"minimum_interest_rate"`
-	MaximumInterestRate               float64  `json:"maximum_interest_rate"`
-	Per                               Per `json:"per"`
-	GraceOnPrincipalPayment           float64  `json:"grace_on_principal_payment"`
-	GraceOnInterestPayment            float64  `json:"grace_on_interest_payment"`
-	GraceOnInterestCharged            float64  `json:"grace_on_interest_charged"`
+	Name                              string              `json:"name"`
+	ShortName                         string              `json:"shortname"`
+	Description                       string              `json:"description"`
+	Fund                              Fund                `json:"fund"`
+	Currency                          Currency            `json:"currency"`
+	DecimalPlaces                     float64             `json:"decimal"`
+	DefaultPrincipal                  float64             `json:"default_principal"`
+	MinimumPrincipal                  float64             `json:"minimum_princial"`
+	MaximumPrincipal                  float64             `json:"maximum_principal"`
+	DefaultLoanTerm                   float64             `json:"default_loan_term"`
+	MinimumLoanTerm                   float64             `json:"minimum_loan_term"`
+	MaximumLoanTerm                   float64             `json:"maximum_loan_term"`
+	RepaymentFrequency                float64             `json:"repayment_frequency"`
+	Type                              RepaymentType       `json:"repayment_type"`
+	DefaultInterestRate               float64             `json:"default_interest_rate"`
+	MinimumInterestRate               float64             `json:"minimum_interest_rate"`
+	MaximumInterestRate               float64             `json:"maximum_interest_rate"`
+	Per                               Per                 `json:"per"`
+	GraceOnPrincipalPayment           float64             `json:"grace_on_principal_payment"`
+	GraceOnInterestPayment            float64             `json:"grace_on_interest_payment"`
+	GraceOnInterestCharged            float64             `json:"grace_on_interest_charged"`
 	InterestMethodology               InterestMethodology `json:"interest_methodology"`
 	AmortizationMethod                AmortizationMethod  `json:"amortization_method"`
-	LoanTransactionProcessingStrategy string  `json:"loan_transaction_processing_strategy"`
-	LoanCharge                        []LoanCharge `gorm:"many2many:loanproduct_loancharges;"`
-	CreditChecks                      string  `json:"credit_checks"`
-	AccountingRule                    AccountingRule `json:"accounting_rule"`
-	AutoDisburse                      AutoDisburse  `json:"auto_disburse"`
-	Status                            Active `json:"product_status"`
+	LoanTransactionProcessingStrategy string              `json:"loan_transaction_processing_strategy"`
+	LoanCharge                        []LoanCharge        `gorm:"many2many:loanproduct_loancharges;"`
+	CreditChecks                      string              `json:"credit_checks"`
+	AccountingRule                    AccountingRule      `json:"accounting_rule"`
+	AutoDisburse                      AutoDisburse        `json:"auto_disburse"`
+	Status                            Active              `json:"product_status"`
 }
 
-
-
-
-//create a loan product 
-func CreateLoanProduct(db *gorm.DB , LoanProduct *LoanProduct)(err error){
+//create a loan product
+func CreateLoanProduct(db *gorm.DB, LoanProduct *LoanProduct) (err error) {
 	err = db.Create(LoanProduct).Error
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 	return nil
 }
+
 //get loan products
 
-func GetLoanProducts(db *gorm.DB,LoanProduct *[]LoanProduct)(err error){
+func GetLoanProducts(db *gorm.DB, LoanProduct *[]LoanProduct) (err error) {
 	err = db.Find(LoanProduct).Error
-	if err!=nil{
-      return nil
+	if err != nil {
+		return nil
 	}
 	return nil
 }
 
 //get loan product by ID
-func GetLoanProduct(db *gorm.DB, LoanProduct *LoanProduct, id string)(err error){
+func GetLoanProduct(db *gorm.DB, LoanProduct *LoanProduct, id string) (err error) {
 	err = db.Where("id = ?", id).First(LoanProduct).Error
-	if err!=nil{
-       return err
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
-//Update loan product 
+//Update loan product
 // still pending research
-func UpdateLoanProduct(db *gorm.DB,LoanProduct *LoanProduct,id string)(err error){
-	db.Where("id =?",id).Save(LoanProduct)
-	//db.Save(LoanProduct)
+func UpdateLoanProduct(db *gorm.DB, LoanProduct *LoanProduct) (err error) {
+	//db.Where("id =?",id).Save(LoanProduct)
+	db.Save(LoanProduct)
 	return nil
 }
 
 //Delete loan Product
-func DeleteLoanProduct(db *gorm.DB,LoanProduct *LoanProduct,id string)(err error){
-   db.Where("id =?",id).Delete(LoanProduct)
-   return nil
+func DeleteLoanProduct(db *gorm.DB, LoanProduct *LoanProduct, id string) (err error) {
+	db.Where("id =?", id).Delete(LoanProduct)
+	return nil
 }
